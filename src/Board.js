aaa2413954c70931274876dfd20d8f7675b9fcc2
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-11-17 16:32:54 +0530

    cells are flipped on click

--- src/Board.js
+++ src/Board.js
@@ -13,25 +13,41 @@ class Board extends Component {
     super(props);
     this.state = {
       hasWon: false,
-      //   arr: this.createBoard(),
+      arr: this.createBoard(),
     };
+    // this.flip = this.flip.bind(this);
+    this.flipCellsAroundMe = this.flipCellsAroundMe.bind(this);
   }
 
-  //   createBoard() {
-  //     let board = [];
+  createBoard() {
+    let board = [];
+    for (let i = 0; i < this.props.trows; i++) {
+      let row = [];
+      for (let j = 0; j < this.props.tcols; j++) {
+        row.push(Math.random() < this.props.chancesLightsStartOn);
+      }
+      board.push(row);
+    }
+    // console.log(board);
+    return board;
+  }
 
-  //     for (var i of this.props.trows) {
-  //       <tr>
-  //         for(var j of this.props.tcols)
-  //         <Cell isLit={true} />
-  //       </tr>;
-  //     }
+  flipCellsAroundMe(x, y) {
+    let arr = this.state.arr;
+    let { trows, tcols } = this.props;
+    // console.log(x, y);
+    function flip(x, y) {
+      if (x >= 0 && x < trows && y >= 0 && y < tcols)
+        //  this.state.arr[x][y] = !arr[x][y];
+        arr[x][y] = !arr[x][y];
+    }
+    flip(x, y);
+    flip(x + 1, y);
+    flip(x - 1, y);
+    flip(x, y + 1);
+    flip(x, y - 1);
 
-  //     return board;
-  //   }
-  turnLight() {
-    if (Math.random() < this.props.chancesLightsStartOn) return true;
-    else return false;
+    this.setState({ arr: arr });
   }
 
   render() {
@@ -39,8 +55,13 @@ class Board extends Component {
     for (let i = 0; i < this.props.trows; i++) {
       let row = [];
       for (let j = 0; j < this.props.tcols; j++) {
+        let position = `${i + 1}-${j + 1}`;
         row.push(
-          <Cell isLit={this.turnLight()} position={`${i + 1}-${j + 1}`} />
+          <Cell
+            flipCells={() => this.flipCellsAroundMe(i, j)}
+            isLit={this.state.arr[i][j]}
+            key={position}
+          />
         );
       }
       loopBoard.push(<tr className="Board-row">{row}</tr>);
--- src/Cell.js
+++ src/Cell.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import "./Cell.css";
 
 class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(ev) {
+    // console.log(ev.which.target);
+    this.props.flipCells();
+  }
+
   render() {
     var classCell = "Cell-data" + (this.props.isLit ? " lit" : "");
 
-    return <td className={classCell}></td>;
+    return <td className={classCell} onClick={this.handleClick}></td>;
   }
 }
 
